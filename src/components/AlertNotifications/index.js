883c9d307ab529c3a3c403d40f93be2cbc72b784
root <shreeshubham23@gmail.com>
2024-01-09 18:49:50 +0000

    Commit: Initialized project with foundational structure and content

--- src/components/AlertNotifications/index.js
+++ src/components/AlertNotifications/index.js
@@ -1,1 +1,38 @@
-// Write your code here
+import './index.css'
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {GrFormClose} from 'react-icons/gr'
+import {MdWarning, MdInfo} from 'react-icons/md'
+import {RiErrorWarningFill} from 'react-icons/ri'
+import Notification from '../Notification'
+
+const AlertNotifications = () => (
+  <>
+    <h1>Alert Notification</h1>
+    <Notification>
+      <AiFillCheckCircle />
+      Success <br /> You can access all the files in the folder
+      <GrFormClose />
+    </Notification>
+
+    <Notification>
+      <RiErrorWarningFill />
+      Error <br /> Sorry, you are not authorized to have access to delete the
+      file
+      <GrFormClose />
+    </Notification>
+
+    <Notification>
+      <MdWarning />
+      Warning <br /> Viewers of this file can see comments and suggestions
+      <GrFormClose />
+    </Notification>
+
+    <Notification>
+      <MdInfo />
+      Info <br /> Anyone on the internet can view these files
+      <GrFormClose />
+    </Notification>
+  </>
+)
+
+export default AlertNotifications
--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,1 +1,20 @@
-// Write your code here
+import './index.css'
+
+const Notification = props => {
+  const {children} = props
+  console.log(props)
+  return (
+    <div className="notification-container">
+      <div className="notification-item-container">
+        <div className="notification-icon-card">{children[0]}</div>
+        <div className="content-card">
+          <p>{children[1]}</p>
+          <p>{children[3]}</p>
+        </div>
+        <div className="delete-icon-card">{children[4]}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Notification
